cba1317bbc6d85077bd05132bc0dff24f6ef70bb
zz85 <zz85nus@gmail.com>
2011-12-10 23:41:35 +0800

    porting more synths from fake plastic cubes

--- synth.js
+++ synth.js
@@ -33,6 +33,84 @@ function SquareGenerator(freq, phase) {
 	return self;
 }
 
+function DescendingSineGenerator(freq, halflifeS) {
+    var self = {'alive': true};
+    var halflife = sampleRate * halflifeS;
+    var t = 0;
+    
+    self.generate = function(buf, offset, count) {
+        for (; count; count--) {
+            var period = sampleRate / (freq * Math.pow(2, -(t / halflife)));
+            var phase = t / period;
+            var result = Math.sin(phase * 2 * Math.PI);
+            buf[offset++] += result;
+            buf[offset++] += result;
+            t++;
+        }
+    }
+    return self;
+}
+
+function NoiseGenerator() {
+    var self = {'alive': true};
+    
+    self.generate = function(buf, offset, count) {
+        for (; count; count--) {
+            buf[offset++] += Math.random();
+            buf[offset++] += Math.random();
+        }
+    }
+    return self;
+}
+
+
+// A Purcussive Envelope
+function AttackDecayGenerator(child, attackTimeS, decayTimeS, amplitude) {
+    var self = {'alive': true}
+    var attackTime = sampleRate * attackTimeS;
+    var decayTime = sampleRate * decayTimeS;
+    var t = 0;
+    
+    self.generate = function(buf, offset, count) {
+        if (!self.alive) return;
+        var input = new Array(count * 2);
+        for (var i = 0; i < count*2; i++) {
+			input[i] = 0;
+		}
+        
+        child.generate(input, 0, count);
+        
+        var childOffset = 0;
+        
+        while (count) {
+            
+            
+            
+            if (t < attackTime) {
+                var localAmplitude = amplitude * (t / attackTime);
+                buf[offset++] += input[childOffset++] * localAmplitude;
+                buf[offset++] += input[childOffset++] * localAmplitude;
+                
+                
+            } else {
+                var localAmplitude = amplitude * (1 - (t - attackTime) / decayTime);
+                buf[offset++] += input[childOffset++] * localAmplitude;
+                buf[offset++] += input[childOffset++] * localAmplitude;
+                
+                if (localAmplitude <= 0) {
+                    self.alive = false;
+                }
+            }
+            
+            t++;
+            count--;
+        }
+        // End generate
+    }
+    return self;
+}
+
+
 function ADSRGenerator(child, attackAmplitude, sustainAmplitude, attackTimeS, decayTimeS, releaseTimeS) {
 	var self = {'alive': true}
 	var attackTime = sampleRate * attackTimeS;
@@ -112,7 +190,7 @@ function midiToFrequency(note) {
 	return 440 * Math.pow(2, (note-69)/12);
 }
 
-PianoProgram = {
+var PianoProgram = {
 	'attackAmplitude': 0.2,
 	'sustainAmplitude': 0.1,
 	'attackTime': 0.02,
@@ -126,7 +204,7 @@ PianoProgram = {
 			this.attackTime, this.decayTime, this.releaseTime
 		);
 	}
-}
+},
 
 StringProgram = {
 	'createNote': function(note, velocity) {
@@ -137,8 +215,9 @@ StringProgram = {
 			0.4, 0.8, 0.4
 		);
 	}
-}
+},
 
+// Piano by default
 PROGRAMS = {
 	41: StringProgram,
 	42: StringProgram,
